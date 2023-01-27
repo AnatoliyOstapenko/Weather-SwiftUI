7fef7667040dcc1f3231775b005b29d2e6b12b40
Anatolii Ostapenko <anatoliy.ostapenko@gmail.com>
2023-01-27 14:32:49 +0200

    Initial Commit

---
+++ Weather SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather SwiftUI
+//
+//  Created by AnatoliiOstapenko on 27.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
